10959b6b45d89c0be954a20353c9caf4f914672e
Nick Eggleston <nickeggleston@yahoo.com>
2024-03-18 13:14:55 -0400

    updated webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -20,21 +17,19 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'J.A.T.E.'
+        title: 'J.A.T.E'
       }),
-
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
         name: 'Just Another Text Editor',
-        short_name: 'J.A.T.E.',
+        short_name: 'J.A.T.E',
         description: 'Takes notes with JavaScript syntax highlighting!',
-        background_color: '#235ca3',
+        background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
         publicPath: '/',
@@ -46,7 +41,6 @@ module.exports = () => {
           },
         ],
       }),
-      
     ],
 
     module: {
@@ -66,7 +60,6 @@ module.exports = () => {
             },
           },
         },
-        
       ],
     },
   };
